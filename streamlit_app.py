6d3787f651e2c83702258b22d513b52cf87ff866
Tommaso Premoli <tommaso.premoli@studenti.unimi.it>
2025-02-03 12:27:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,8 @@ if ingredient_list:
 # new section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_responde.json(), use_container_width=True)
 
 
 
