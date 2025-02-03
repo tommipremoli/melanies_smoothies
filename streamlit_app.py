e5c2a3d4db8c29a03b9ebcf42162d7662f6e29bd
Tommaso Premoli <tommaso.premoli@studenti.unimi.it>
2025-02-03 12:24:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,8 @@ if ingredient_list:
 
 # new section to display smoothiefroot nutrition information
 import requests
-smoothie_froot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothie_froot_response)
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
